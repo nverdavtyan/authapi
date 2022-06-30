4457f33425be0b08d25e52982faf4f85327150f6
nverdavtyan <nverdavtyan1994@gmail.com>
2022-06-30 21:51:59 +0200

    [ADD] création api backend register ok

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+class UserController extends Controller
+{
+    function create(Request $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'lastname' => $data['lastname'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+}
